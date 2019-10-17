b0f192fbf26aab91b69ac63ad9341f193479f4f9
FatimaAminu <52076049+FatimaAminu@users.noreply.github.com>
2019-10-17 12:16:52 -0700

    done with the project

---
+++ script.js
@@ -0,0 +1,24 @@
+   
+    for (var i=1; i<=100; i++){
+        if( (i%3 == 0) && (i%5 == 0) ){
+            document.write("FIZZBUZZ");
+        }
+        else if (i%3 == 0){
+            document.write("Fizz");
+        }
+        else  if (i%5 == 0){
+            document.write("Buzz");
+        }
+        
+        else{
+            document.write(i);
+        }
+    }
+
+
+
+
+
+   
+
+   
